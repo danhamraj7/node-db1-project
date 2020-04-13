e609e3a95874d930c8c4f792bd9500f3560bf2b1
danhamraj <danhamraj@gmail.com>
2020-04-13 17:58:34 -0400

    Created CRUD for accounts router

--- accounts/accountsModel.js
+++ accounts/accountsModel.js
@@ -1,14 +1,30 @@
 const db = require("../data/dbConfig");
 module.exports = {
   find,
-  //   findById,
-  //   findSteps,
-  //   add,
-  //   addStep,
-  //   update,
-  //   remove
+  findById,
+  insert,
+  update,
+  remove,
 };
 
 function find() {
   return db("accounts");
+}
+
+function findById(id) {
+  return db("accounts").where({ id }).first();
+}
+
+function insert(account) {
+  return db("accounts")
+    .insert(account, "id")
+    .then((ids) => ({ id: ids[0] }));
+}
+
+function update(account, id) {
+  return db("accounts").where("id", Number(id)).update(account);
+}
+
+function remove(id) {
+  return db("accounts").where("id", Number(id)).del();
 }
--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const accounts = require("./accountsModel");
+const db = require("../data/dbConfig");
 
 const router = express.Router();
 
@@ -14,6 +15,82 @@ router.get("/", (req, res) => {
         .status(500)
         .json({ error: "The accounts information could not be retrieved." })
     );
+});
+
+// get an account by acc id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  accounts
+    .findById(id)
+    .then((account) => {
+      if (account) {
+        res.json(account);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find account with given id." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get account" });
+    });
+});
+
+// add an account
+router.post("/", (req, res) => {
+  const newPost = req.body;
+  //console.log(newPost);
+  accounts
+    .insert(newPost)
+    .then((result) => res.status(201).json(result))
+    .catch((err) =>
+      res.status(400).json({
+        errorMessage: "Please provide name and budget for the account.",
+      })
+    );
+});
+
+//update account
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  accounts
+    .findById(id)
+    .then((account) => {
+      if (account) {
+        accounts.update(changes, id).then((updatedAccount) => {
+          res.json({ updated: updatedAccount });
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find account with given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to update account" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  accounts
+    .remove(id)
+    .then((deleted) => {
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find account with given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to delete account" });
+    });
 });
 
 module.exports = router;
